b3882eafdf431a7cb86a30ccff8e48ba26673094
MasterEyup <eyuptrabzonn@gmail.com>
2022-02-16 11:57:02 +0100

    Login page done, started with Home page

---
+++ src/components/Home/HomePage.tsx
@@ -0,0 +1,22 @@
+import React from "react";
+import "../../css/index.css"
+import NavBar from "../static-parts/NavBar";
+export default class HomePage extends React.Component{
+
+    constructor(props ={}){
+        super(props);
+    }
+
+    render(){
+        return(<div className="flex flex-col justify-center items-center h-screen">
+        <div className="box-border h-72 w-auto h-auto p-4 border-4 bg-slate-200	-500 m4 rounded ">
+            <div className="flex-1 h-full max-w-4xl mx-auto bg-white rounded-lg shadow-xl">
+                <div className="flex flex-col md:flex-row">
+                  <NavBar/>
+                    </div>
+                    </div>
+                    </div>
+                    </div>
+        )
+    }
+}
---
+++ src/components/Login/LoginPage.tsx
@@ -0,0 +1,37 @@
+import React from "react";
+import "../../css/index.css"
+import face_icon1 from "../../images/recognition_1_face.png" 
+export default class LoginPage extends React.Component{
+
+    constructor(props ={}){
+        super(props);
+    }
+
+    render(){
+        return(<div className="flex flex-col justify-center items-center h-screen">
+        <div className="box-border h-72 w-auto h-auto p-4 border-4 bg-slate-200	-500 m4 rounded ">
+            <div className="flex-1 h-full max-w-4xl mx-auto bg-white rounded-lg shadow-xl">
+                <div className="flex flex-col md:flex-row">
+                    <div className="h-32 md:h-auto md:w-1/2">
+                        <img className="object-cover w-full h-full" src="https://res.cloudinary.com/canonical/image/fetch/f_auto,q_auto,fl_sanitize,w_645,h_645/https://dashboard.snapcraft.io/site_media/appmedia/2019/11/puploc_feat.png"
+                            alt="img" />
+                    </div>
+        <div className='flex items-center justify-center p-6 sm:p-12 md:w-1/2'>  
+        <div className="w-full">      
+        <div className="mb-6">
+        <h2 className="flex justify-center text-2xl font-bold text-center text-gray-700"><img width="32" height="32" src={face_icon1} draggable={false}></img>&nbsp; Admin Login</h2></div>
+        <div className="mb-6">
+        <label htmlFor="username" className="text-base font-normal">Username</label>
+        <input className="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600" 
+        name="username" placeholder="Username" id="username" type="text"></input></div>
+        <div className="mb-6">
+        <label htmlFor="password" className="text-base font-normal">Password</label><br/>
+        <input className="w-full px-4 py-2 text-sm border rounded-md focus:border-blue-400 focus:outline-none focus:ring-1 focus:ring-blue-600"  name="password" placeholder="Your password.." id="password" type="password"></input>
+        </div>
+        <div className="mb-4">
+        <input type="button" style={{cursor:"pointer"}} className="w-full bg-blue-200 hover:bg-blue-300 text-white font-bold py-2 px-4 border border-blue-300 rounded" value={"Login"}></input>
+        </div>
+        </div></div></div></div></div></div>
+        )
+    }
+}
