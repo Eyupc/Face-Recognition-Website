8b2731e537fe649bc678377b1897e51ca316f5fc
MasterEyup <eyuptrabzonn@gmail.com>
2022-02-17 10:05:00 +0100

    Navbar done, added statscard

--- src/components/Home/HomePage.tsx
+++ src/components/Home/HomePage.tsx
@@ -1,6 +1,7 @@
 import React from "react";
 import "../../css/index.css"
 import NavBar from "../static-parts/NavBar";
+import { StatsCard } from "./parts/StatsCard";
 export default class HomePage extends React.Component{
 
     constructor(props ={}){
@@ -11,8 +12,13 @@ export default class HomePage extends React.Component{
         return(<div className="flex flex-col justify-center items-center h-screen">
         <div className="box-border h-72 w-auto h-auto p-4 border-4 bg-slate-200	-500 m4 rounded ">
             <div className="flex-1 h-full max-w-4xl mx-auto bg-white rounded-lg shadow-xl">
-                <div className="flex flex-col md:flex-row">
+            <div className="flex flex-col md:flex-col">
+            <div className="mb-6">
                   <NavBar/>
+            </div>
+            <div className="mb-4">
+                  <StatsCard/>
+                  </div>
                     </div>
                     </div>
                     </div>
--- src/components/static-parts/NavBar.tsx
+++ src/components/static-parts/NavBar.tsx
@@ -1,33 +1,37 @@
 import React from "react";
+import "../../css/index.css"
 import icon from "../../images/recognition_2_face.png"
+import "flowbite"
 export default class NavBar extends React.Component{
 
     constructor(props = {}){
         super(props);
     }
 
+
     render() {
-        return(<nav className="bg-white border-gray-200 px-2 sm:px-4 py-2.5 rounded dark:bg-gray-800">
-        <div className="container flex flex-wrap justify-between items-center mx-auto">
-        <a href="#" className="flex">
-        <span className="self-center text-lg font-semibold whitespace-nowrap dark:text-white"><img src={icon}></img>Face Recognition</span></a>
-        <div className="hidden justify-between items-center w-full md:flex md:w-auto md:order-1" id="mobile-menu-4">
-          <ul className="flex flex-col mt-4 md:flex-row md:space-x-8 md:mt-0 md:text-sm md:font-medium">
-            <li>
-              <a href="#" className="block py-2 pr-4 pl-3 text-white bg-blue-700 rounded md:bg-transparent md:text-blue-700 md:p-0 dark:text-white" aria-current="page">Home</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">About</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Services</a>
-            </li>
-            <li>
-              <a href="#" className="block py-2 pr-4 pl-3 text-gray-700 border-b border-gray-100 hover:bg-gray-50 md:hover:bg-transparent md:border-0 md:hover:text-blue-700 md:p-0 md:dark:hover:text-white dark:text-gray-400 dark:hover:bg-gray-700 dark:hover:text-white md:dark:hover:bg-transparent dark:border-gray-700">Contact</a>
-            </li>
-          </ul>
+        return(	<nav className="bg-blue-200 border-gray-200 px-2">
+        <div className="container mx-auto flex flex-wrap items-center justify-between">
+          <a href="#" className="flex">
+            <img src={icon}/>
+              <span className="self-center text-lg font-semibold whitespace-nowrap">Face Recognition</span>
+          </a>
+          <button data-collapse-toggle="mobile-menu" type="button" className="md:hidden ml-3 text-gray-400 hover:text-gray-900 focus:outline-none focus:ring-2 focus:ring-blue-300 rounded-lg inline-flex items-center justify-center" aria-controls="mobile-menu-2" aria-expanded="false">
+            <svg className="w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M3 5a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 10a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1zM3 15a1 1 0 011-1h12a1 1 0 110 2H4a1 1 0 01-1-1z" clipRule="evenodd"></path></svg>
+            <svg className="hidden w-6 h-6" fill="currentColor" viewBox="0 0 20 20" xmlns="http://www.w3.org/2000/svg"><path fillRule="evenodd" d="M4.293 4.293a1 1 0 011.414 0L10 8.586l4.293-4.293a1 1 0 111.414 1.414L11.414 10l4.293 4.293a1 1 0 01-1.414 1.414L10 11.414l-4.293 4.293a1 1 0 01-1.414-1.414L8.586 10 4.293 5.707a1 1 0 010-1.414z" clipRule="evenodd"></path></svg>
+          </button>
+          <div className="hidden md:block w-full md:w-auto p-4" id="mobile-menu">
+            <ul className="flex-col md:flex-row flex md:space-x-8 mt-4 md:mt-0 md:text-sm md:font-medium">
+              <li>
+                <a href="#" className="bg-blue-700 md:bg-transparent text-white block pl-3 pr-4 py-2 md:text-blue-700 md:p-0 rounded" aria-current="page">Home</a>
+              </li>
+              <li>
+                <a href="#" className="text-gray-700 hover:bg-gray-50 border-b border-gray-100 md:hover:bg-transparent md:border-0 block pl-3 pr-4 py-2 md:hover:text-blue-700 md:p-0">Test</a>
+              </li>
+            </ul>
+          </div>
         </div>
-        </div>
-      </nav>)
+      </nav>
+          );
     }
 }
