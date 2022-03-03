93a8248810586db6a9302d0c2ca88e94b9a6edcc
unknown <eyuptrabzonn@gmail.com>
2022-03-04 00:47:26 +0100

    small changes

--- src/components/Manage/AddUserPage.tsx
+++ src/components/Manage/AddUserPage.tsx
@@ -4,23 +4,45 @@ import NavBar from "../static-parts/NavBar";
 import imageCompression from 'browser-image-compression';
 
 
-export class AddUserPage extends React.Component {
+type State = {
+  user_added_alert:boolean,
+  added:boolean,
+}
+export class AddUserPage extends React.Component<{},State> {
   
   private images_encoded: string[] = [];
   private reader = new FileReader();
   private WSClient:WSClient;
   private ws:WebSocket;
 
+
   constructor(props={}){
         super(props)
-        this.WSClient = new WSClient("localhost",7777,false,"AddUserPage");
+        this.WSClient = new WSClient("192.168.0.155",7777,false,"AddUserPage");
         this.ws = this.WSClient.websocket;
         this.ws.onmessage = (msg) => this.onMessage(msg)
+        this.state = {
+          user_added_alert:false,
+          added:false
+        }
     }
     
     onMessage(msg:any){
-      
+      let data = JSON.parse(this.WSClient.decoder.decode(msg.data));
+      if(data.header == "AddUserEvent"){
+        if(Number(data.data[0].faces_count) > 0){
+        this.setState({
+          user_added_alert:true,
+          added:true
+        })
+      }else{
+      this.setState({
+        user_added_alert:true,
+        added:false
+      })
     }
+  }
+  }
 
     SendData(json:string){
       if(this.ws.OPEN === this.ws.readyState)
@@ -49,7 +71,7 @@ export class AddUserPage extends React.Component {
           img = data.replace("data:", "").replace(/^.+,/, "");
 
           this.images_encoded.push(img);
-          document.getElementById("img")!.setAttribute("src",'data:image/jpg;base64,'+img)
+         // document.getElementById("img")!.setAttribute("src",'data:image/jpg;base64,'+img)
           //console.log(this.state.images_encoded)
 
           //document.getElementById("image")!.setAttribute("src",img);
@@ -58,7 +80,7 @@ export class AddUserPage extends React.Component {
       }
     }
 
-       getBase64(file:any) { //TODO -> image size reduce
+       getBase64(file:any) { 
         return new Promise((resolve, reject) => {
           const reader = this.reader;
           reader.readAsDataURL(file);
@@ -68,6 +90,7 @@ export class AddUserPage extends React.Component {
       }
 
       onSubmit(e:any){
+        e.preventDefault();
         if(this.images_encoded.length == 0)
           return;
 
@@ -86,7 +109,13 @@ export class AddUserPage extends React.Component {
         (document.getElementById("lastname") as HTMLInputElement)!.value = "";
         (document.getElementById("age") as HTMLInputElement)!.value = "";
         (document.getElementById("files") as HTMLInputElement)!.value = "";
-        e.preventDefault();
+      }
+
+      PopUpClose(){
+        this.setState({
+          user_added_alert:false,
+          added:false,
+        })
       }
 
     render(){
@@ -98,8 +127,24 @@ export class AddUserPage extends React.Component {
                   <NavBar/>
 
   <div className="md:w-auto p-4">
+  {this.state.user_added_alert ? this.state.added ?  <div className="mb-4">
+  <div className="bg-green-100 border border-green-400 text-green-700 px-4 py-3 rounded relative" role="alert">
+  <strong className="font-bold">[OK]</strong>
+  <span className="block sm:inline">The user has been added successfully!</span>
+  <span onClick={()=>this.PopUpClose()} className="absolute top-0 bottom-0 right-0 px-4 py-3">
+    <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+  </span>
+</div></div> : 
+<div className="mb-4">
+  <div className="bg-red-100 border border-red-400 text-red-700 px-4 py-3 rounded relative" role="alert">
+  <strong className="font-bold">[ERROR]</strong>
+  <span className="block sm:inline">No faces were found in these photos!</span>
+  <span onClick={()=>this.PopUpClose()} className="absolute top-0 bottom-0 right-0 px-4 py-3">
+    <svg className="fill-current h-6 w-6 text-red-500" role="button" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 20 20"><title>Close</title><path d="M14.348 14.849a1.2 1.2 0 0 1-1.697 0L10 11.819l-2.651 3.029a1.2 1.2 0 1 1-1.697-1.697l2.758-3.15-2.759-3.152a1.2 1.2 0 1 1 1.697-1.697L10 8.183l2.651-3.031a1.2 1.2 0 1 1 1.697 1.697l-2.758 3.152 2.758 3.15a1.2 1.2 0 0 1 0 1.698z"/></svg>
+  </span>
+</div></div>
+: <></>}
   <div className="md:flex md:items-center mb-6">
-
 <div className="md:w-full">
 <label className="block text-gray-500 font-bold md:text-left mb-1 md:mb-0 pr-4" htmlFor="Name">
         Name:
@@ -138,7 +183,6 @@ type="number" min="1" name="Age" required/>
 <div className="md:w-1/6">
 <input style={{cursor:"pointer"}} className="w-64 h-10 px-5 m-2 text-blue-100 transition-colors duration-150 bg-blue-400 rounded-lg focus:shadow-outline hover:bg-blue-500" type="submit" value="Verander!"/>
 </div></div>
-<img src="" id="img"></img>
 </div>
                       </div>
                   </div></div></div></form>
