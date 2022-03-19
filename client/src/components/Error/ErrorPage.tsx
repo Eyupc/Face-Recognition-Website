872fbaec6d1a6254ccbcbb4f32dff2787a62106f
unknown <eyuptrabzonn@gmail.com>
2022-03-19 23:54:13 +0100

    Update ErrorPage.tsx

--- client/src/components/Error/ErrorPage.tsx
+++ client/src/components/Error/ErrorPage.tsx
@@ -2,43 +2,40 @@ import React from "react";
 import { Navigate } from "react-router-dom";
 import error_img from "../../images/error_fc.jpg";
 
-
 export class ErrorPage extends React.Component {
-
   state = {
-    goBack:false,
+    goBack: false,
+  };
+  constructor(props = {}) {
+    super(props);
   }
-    constructor(props={}){
-        super(props);
 
-    }
+  render() {
+    if (this.state.goBack) return <Navigate to="/" replace />;
 
-    render(){
-
-      if(this.state.goBack)
-      return <Navigate to="/" replace/>
-
-        return(<div className="flex items-center justify-center w-screen h-screen">
+    return (
+      <div className="flex items-center justify-center w-screen h-screen">
         <div className="px-4 lg:py-12">
           <div className="lg:gap-4 lg:flex">
-            <div
-              className="flex flex-col items-center justify-center md:py-24 lg:py-32"
-            >
+            <div className="flex flex-col items-center justify-center md:py-24 lg:py-32">
               <h1 className="font-bold text-purple-600 text-9xl">404</h1>
-              <p
-                className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl"
-              >
+              <p className="mb-2 text-2xl font-bold text-center text-gray-800 md:text-3xl">
                 <span className="text-red-600">Oops!</span> Page not found
               </p>
               <p className="mb-8 text-center text-gray-500 md:text-lg">
                 The page you’re looking for doesn’t exist.
               </p>
-              <div onClick={()=>this.setState({goBack:true})} 
+              <div
+                onClick={() => this.setState({ goBack: true })}
                 className="px-6 py-2 text-sm font-semibold text-blue-800 bg-blue-100 hover:bg-blue-300"
-                style={{cursor:"pointer" }}>Go Back</div>
+                style={{ cursor: "pointer" }}
+              >
+                Go Back
+              </div>
             </div>
             <div className="mt-4">
-              <img draggable="false"
+              <img
+                draggable="false"
                 src={error_img}
                 alt="img"
                 className="object-cover w-full h-full"
@@ -46,7 +43,7 @@ export class ErrorPage extends React.Component {
             </div>
           </div>
         </div>
-      </div>)
-    }
-
+      </div>
+    );
+  }
 }
