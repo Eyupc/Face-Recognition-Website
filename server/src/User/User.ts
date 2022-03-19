f0c506ee9c1b32a9e1e626bde1b85a4cdddf2ea4
unknown <eyuptrabzonn@gmail.com>
2022-03-19 23:57:23 +0100

    Update User.ts

--- server/src/User/User.ts
+++ server/src/User/User.ts
@@ -1,9 +1,11 @@
 export class User {
     private _id:number;
     private _username:string;
-    constructor(id:number,username:string){
+    private _rank:number;
+    constructor(id:number,username:string,rank:number){
         this._id = id;
         this._username = username;
+        this._rank = rank;
     }
 
     public get id(){
@@ -11,5 +13,8 @@ export class User {
     }
     public get username(){
         return this._username;
+    }
+    public get rank(){
+        return this._rank;
     }
 }
