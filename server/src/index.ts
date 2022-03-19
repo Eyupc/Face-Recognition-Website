f0555675e843c87b75e5381a327094814c781276
unknown <eyuptrabzonn@gmail.com>
2022-03-19 23:57:21 +0100

    Update index.ts

--- server/src/index.ts
+++ server/src/index.ts
@@ -2,12 +2,16 @@ import DatabaseService from "./Database/DatabaseService";
 import { HTTPServer } from "./HTTP/HTTPServer";
 
 export class Main {
-public static http = new HTTPServer("0.0.0.0",8080)
-public static databaseService = new DatabaseService("0.0.0.0",27017,"face-recognition")
+  public static http = new HTTPServer("0.0.0.0", 8080);
+  public static databaseService = new DatabaseService(
+    "0.0.0.0",
+    27017,
+    "face-recognition"
+  );
 
-constructor(){
-    Main.http.Start()
-    Main.databaseService.connect()
+  constructor() {
+    Main.http.Start();
+    Main.databaseService.connect();
+  }
 }
-}
-var main = new Main()
+var main = new Main();
