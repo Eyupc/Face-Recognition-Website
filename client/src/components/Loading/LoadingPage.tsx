8d6b46403abd12d6c94ffd5a120f04887854e858
unknown <eyuptrabzonn@gmail.com>
2022-03-19 23:54:32 +0100

    Update LoadingPage.tsx

--- client/src/components/Loading/LoadingPage.tsx
+++ client/src/components/Loading/LoadingPage.tsx
@@ -2,18 +2,24 @@ import React from "react";
 import ReactLoading from "react-loading";
 
 export default class ExportPage extends React.Component {
+  constructor(props = {}) {
+    super(props);
+  }
 
-    constructor(props={}){
-        super(props);
-    }
-
-    render(){
-       return( <div className="bg-[#ededed] flex flex-row justify-center items-center h-screen"> <ReactLoading
-        type={"spin"}
-        color={"#rgb(40 58 81)"}
-        height={100}
-        width={100}
-      /> <p className="p-8 flex justify-center text-2xl font-bold text-center text-gray-700">Loading.....</p></div>
-        )
-    }
+  render() {
+    return (
+      <div className="bg-[#ededed] flex flex-row justify-center items-center h-screen">
+        {" "}
+        <ReactLoading
+          type={"spin"}
+          color={"#rgb(40 58 81)"}
+          height={100}
+          width={100}
+        />{" "}
+        <p className="p-8 flex justify-center text-2xl font-bold text-center text-gray-700">
+          Loading.....
+        </p>
+      </div>
+    );
+  }
 }
